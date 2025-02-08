da5a43c981fd8824984e4cc88e49e2280151c5d9
JoowJhony <jonatasrosalvo@gmail.com>
2025-02-08 00:08:58 -0300

    python e venv pelo ansible

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -29,6 +29,14 @@ resource "aws_vpc_security_group_ingress_rule" "ec2_inbound_http" {
   to_port           = 8080
 }
 
+resource "aws_vpc_security_group_ingress_rule" "ec2_inbound_http2" {
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8000
+  ip_protocol       = "tcp"
+  to_port           = 8000
+}
+
 resource "aws_vpc_security_group_ingress_rule" "ec2_inbound_icmp" {
   security_group_id = aws_security_group.ec2_sg.id
   cidr_ipv4         = "0.0.0.0/0"
@@ -46,7 +54,7 @@ resource "aws_instance" "ubuntu_ec2" {
   availability_zone           = var.availability_zone
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   subnet_id                   = var.public_subnet_id
-#  key_name = aws_key_pair.key_pair_ubuntu_ec2.key_name
+  key_name = aws_key_pair.key_pair_ubuntu_ec2.key_name
 #  user_data = <<-EOF
 #		 #!/bin/bash
 #		 cd /home/ubuntu
@@ -58,7 +66,7 @@ resource "aws_instance" "ubuntu_ec2" {
   }
 }
 
-#resource "aws_key_pair" "key_pair_ubuntu_ec2" {
-#  key_name   = "ubuntu_kp"
-#  public_key = file("/home/joow/.ssh/ubuntu_kp.pub")
-#}
+resource "aws_key_pair" "key_pair_ubuntu_ec2" {
+  key_name   = "ubuntu_kp"
+  public_key = file("/home/joow/.ssh/ubuntu_kp.pub")
+}
